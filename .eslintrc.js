5da09e76fae150572a74a4d15be567a0cbd71d54
Jack Cherng <jfcherng@gmail.com>
2020-04-24 17:13:10 +0800

    chore: nits
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,26 @@
 module.exports = {
-  "env": {
-    "es6": true,
-    "browser": true,
-    "node": true,
+  env: {
+    es6: true,
+    browser: true,
+    node: true,
   },
-  "parserOptions": {
-    "ecmaFeatures": {
-      "jsx": true,
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    "ecmaVersion": 2018,
-    "sourceType": "module",
+    ecmaVersion: 2018,
+    sourceType: 'module',
   },
-  "globals": {
-    "$": true,
-    "jQuery": true,
+  globals: {
+    $: true,
+    jQuery: true,
   },
-  "extends": [
-    "eslint:recommended",
-    "eslint-config-prettier",
-  ],
-  "plugins": [
-    "prettier"
-  ],
-  "rules": {
-    "prettier/prettier": ["error"],
-    "no-trailing-spaces": ["warn"],
-    "no-unused-vars": ["warn"],
-    "semi": ["error"],
+  extends: ['eslint:recommended', 'eslint-config-prettier'],
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': ['error'],
+    'no-trailing-spaces': ['warn'],
+    'no-unused-vars': ['warn'],
+    semi: ['error'],
   },
 };
