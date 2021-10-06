a060cddc90b43315a75309e86c70f85352f1aa5d
Jack Cherng <jfcherng@gmail.com>
2021-10-06 11:00:03 +0800

    chore: update php-cs-fixer v3
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,7 +1,7 @@
 <?php
 
 $config = (new PhpCsFixer\Config())
-    ->setIndent("    ")
+    ->setIndent('    ')
     ->setLineEnding("\n")
     ->setCacheFile(__DIR__ . '/.php_cs.cache')
     ->setRiskyAllowed(true)
@@ -22,18 +22,18 @@ $config = (new PhpCsFixer\Config())
         'comment_to_phpdoc' => true,
         'compact_nullable_typehint' => true,
         'concat_space' => ['spacing' => 'one'],
+        'echo_tag_syntax' => ['format' => 'short'],
         'escape_implicit_backslashes' => false,
         'fully_qualified_strict_types' => true,
         'linebreak_after_opening_tag' => true,
         'list_syntax' => ['syntax' => 'short'],
-        'method_argument_space' => ['ensure_fully_multiline' => true],
-        'native_constant_invocation' => true,
-        'native_function_invocation' => true,
+        'method_argument_space' => ['on_multiline' => 'ensure_fully_multiline'],
+        'multiline_whitespace_before_semicolons' => ['strategy' => 'new_line_for_chained_calls'],
+        'native_constant_invocation' => ['scope' => 'all'],
+        'native_function_invocation' => ['scope' => 'all', 'include' => ['@compiler_optimized']],
         'native_function_type_declaration_casing' => true,
         'no_alternative_syntax' => true,
-        'no_multiline_whitespace_before_semicolons' => true,
         'no_null_property_initialization' => true,
-        'no_short_echo_tag' => true,
         'no_superfluous_elseif' => true,
         'no_unneeded_control_parentheses' => true,
         'no_useless_else' => true,
@@ -43,12 +43,12 @@ $config = (new PhpCsFixer\Config())
         'ordered_class_elements' => true,
         'ordered_imports' => ['sort_algorithm' => 'alpha', 'imports_order' => ['class', 'const', 'function']],
         'ordered_interfaces' => true,
-        'php_unit_ordered_covers' => true,
         'php_unit_set_up_tear_down_visibility' => true,
         'php_unit_strict' => true,
         'php_unit_test_class_requires_covers' => true,
         'phpdoc_add_missing_param_annotation' => true,
         'phpdoc_order' => true,
+        'phpdoc_order_by_value' => ['annotations' => ['covers']],
         'phpdoc_to_comment' => false,
         'phpdoc_types_order' => true,
         'pow_to_exponentiation' => true,
@@ -76,7 +76,6 @@ $config = (new PhpCsFixer\Config())
             ->exclude('vendor')
             // frontend
             ->exclude('node_modules')
-    )
-;
+    );
 
 return $config;
