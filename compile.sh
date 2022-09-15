916a15189059c22ee11a94dbe3e131d9a3c8c76e
Jack Cherng <jfcherng@gmail.com>
2022-09-16 00:43:39 +0800

    style: shfmt
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- compile.sh
+++ compile.sh
@@ -6,7 +6,7 @@
 # Author: Jack Cherng <jfcherng@gmail.com>         #
 #--------------------------------------------------#
 
-SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 THREAD_CNT=$(getconf _NPROCESSORS_ONLN)
 PROJECT_ROOT=${SCRIPT_DIR}
 
@@ -24,13 +24,11 @@ JS_FILES=(
     "assets/pages/settings.js"
 )
 
-
 #-------#
 # begin #
 #-------#
 
 pushd "${SCRIPT_DIR}" || exit
-
 
 #--------------------#
 # compile LESS files #
@@ -48,12 +46,11 @@ for file_src in "${LESS_FILES[@]}"; do
 
     file_dst=${file_src%.*}.css
 
-    npx lessc --insecure "${file_src}" \
-        | printf "%s\n" "$(cat -)" \
-        | npx cleancss -O2 -f 'breaks:afterAtRule=on,afterBlockBegins=on,afterBlockEnds=on,afterComment=on,afterProperty=on,afterRuleBegins=on,afterRuleEnds=on,beforeBlockEnds=on,betweenSelectors=on;spaces:aroundSelectorRelation=on,beforeBlockBegins=on,beforeValue=on;indentBy:2;indentWith:space;breakWith:lf' \
-        > "${file_dst}"
+    npx lessc --insecure "${file_src}" |
+        printf "%s\n" "$(cat -)" |
+        npx cleancss -O2 -f 'breaks:afterAtRule=on,afterBlockBegins=on,afterBlockEnds=on,afterComment=on,afterProperty=on,afterRuleBegins=on,afterRuleEnds=on,beforeBlockEnds=on,betweenSelectors=on;spaces:aroundSelectorRelation=on,beforeBlockBegins=on,beforeValue=on;indentBy:2;indentWith:space;breakWith:lf' \
+            >"${file_dst}"
 done
-
 
 #----------------------------#
 # transpile Javascript files #
@@ -74,21 +71,20 @@ for file_src in "${JS_FILES[@]}"; do
 
     if [ ! -f "${file_export}" ]; then
         has_no_file_export=true
-        echo ";" > "${file_export}"
+        echo ";" >"${file_export}"
     fi
 
     # to make the output file more diff-friendly, we beautify it and remove leading spaces
-    cat "${file_src}" "${file_export}" \
-        | npx browserify -t [ babelify ] - \
-        | npx terser --config-file terser.json -- \
-        | sed -e 's/[[:space:]]+$//' \
-        > "${file_dst}"
+    cat "${file_src}" "${file_export}" |
+        npx browserify -t [ babelify ] - |
+        npx terser --config-file terser.json -- |
+        sed -e 's/[[:space:]]+$//' \
+            >"${file_dst}"
 
     if [ "${has_no_file_export}" = "true" ]; then
         rm -f "${file_export}"
     fi
 done
-
 
 #-----#
 # end #
